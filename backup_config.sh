fb851eabf994453b8eea205b3912f55453ecbcdb
Christian Amezcua <33039101+chrisamz@users.noreply.github.com>
2024-07-01 13:33:03 -0700

    Create backup_config.sh

---
+++ backup_config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# PostgreSQL configuration
+PG_HOST="your_pg_host"
+PG_PORT="your_pg_port"
+PG_USER="your_pg_user"
+PG_DATABASE="your_pg_database"
+BACKUP_DIR="/path/to/your/backup/dir"
+
+# AWS S3 configuration
+S3_BUCKET="your_s3_bucket_name"
