69b393799df6a6fc8ce5994a32070bdeb89b9221
Christian Amezcua <33039101+chrisamz@users.noreply.github.com>
2024-07-01 13:34:46 -0700

    Create aws_config.py

---
+++ aws_config.py
@@ -0,0 +1,6 @@
+# AWS configuration
+
+AWS_ACCESS_KEY = 'your_aws_access_key'
+AWS_SECRET_KEY = 'your_aws_secret_key'
+S3_BUCKET_NAME = 'your_s3_bucket_name'
+BACKUP_DIR = '/path/to/your/backup/dir'
